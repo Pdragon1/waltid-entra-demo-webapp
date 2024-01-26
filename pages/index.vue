cf87b4dfce8f3ec12b4327f0dc783c4a102e9f24
waltkb <68587968+waltkb@users.noreply.github.com>
2024-01-26 03:34:24 +0100

    Link to issuance & verification site on entra webapp

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,9 @@
 <template>
-    Hello world
+    <div class="p-4 flex flex-col gap-y-1">
+        <p><NuxtLink to="issuance" class="underline">Issuance</NuxtLink></p>
+        <p><NuxtLink to="verification" class="underline">Verification</NuxtLink></p>
+    </div>
+
 </template>
 
 <script setup lang="ts">
